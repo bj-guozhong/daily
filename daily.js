a1ef61b242717a9c3c4eacff3a520289f345608d
gz <74884824+bj-guozhong@users.noreply.github.com>
2020-12-23 16:36:55 +0800

    全屏模式下无法监听esc按键

--- daily.js
+++ daily.js
@@ -23,7 +23,7 @@ $(document).ready(function() {
 });
 </script>
 
-3.全屏模式下无法监听esc按键，解决办法：onresize 监听窗口改变
+3.全屏模式下无法监听esc按键，键盘ESC代码27不生效，被win10自带的ESC覆盖也可用此方法：onresize 监听窗口改变.
 window.onresize = function() {
   if (!checkFull()) {
     //清空全屏模式下的水印内容，方法watermark3
