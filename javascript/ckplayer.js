ba3a5e7e9e98488096dd82ccf1675a01b711e474
gz <74884824+bj-guozhong@users.noreply.github.com>
2020-12-30 14:31:01 +0800

    学习使用新JS视频控件ckplayer

---
+++ javascript/ckplayer.js
@@ -0,0 +1,10 @@
+1.第三方JS播放器插件，地址：https://www.ckplayer.com/demo.html
+
+2.可进行下载源码使用,因原PKplayer不支持直播，所以改用此控件，方便以后扩展。
+
+3.亦可在此控件中的ckplayer.js内 fullScreen: function() { 方法加视频全屏水印显示
+
+watermark3({"watermarl_element":this.PD, "watermark_txt":document.getElementById("watermarkId").value});//当用户全屏视频时添加水印 add by gz 20201230
+
+watermark3()方法来源water2js,可以在仓库videoWaterMarkByJsDemo中找到。
+
